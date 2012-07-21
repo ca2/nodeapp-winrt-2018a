4be8a1abfcfdbaba4012d640bca82676ad310211
ca2 <camilo@ca2.email>
2012-07-21 13:54:19 +0000

    Eu te amo Thommy, Thomas Borregaard Sørensen!!

---
+++ os/win_internal.h
@@ -0,0 +1,21 @@
+#pragma once
+
+
+#undef new
+#include <GdiPlus.h>
+#include <Gdipluseffects.h>
+
+#define new DEBUG_NEW
+
+
+#include "ca/radix/implementation.h"
+#include "dir.h"
+#include "WFileFind.h"
+#include "WinFileSystem.h"
+#include "WinFile.h"
+#include "WindowsShell.h"
+//#include "ca2/ex1_resource.h"
+#include "WinResource.h"
+#include "application.h"
+#include "WinStdioFile.h"
+#include "WinFileSet.h"

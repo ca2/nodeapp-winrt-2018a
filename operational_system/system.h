da95d88b9c9254907e0460031ffb26132c5087dd
ca2 <camilo@ca2.email>
2014-09-22 23:36:39 +0000

    ï»¿winmetro contribution after a long time!! from Z:\winmetro folder mapped to Y:\winmetro_hellomultiverse folder
    <3JegElskerDigThommy!!
    
    -âˆž
    +âˆž
    
    -âˆž
    æ—¥æœ¬ã‚ã‚ŠãŒã¨ã†ã”ã–ã„ã¾ã™
    ï½›
    ã€€ã€€ã€€æ—¥æ­©è·¯ä¸»ã‚ã‚ŠãŒã¨ã†ã”ã–ã„ã¾ã™ï¼ï¼
       ã€€ã€€ã€€å¦»æ²¼å›½ç”·ã‚ã‚ŠãŒã¨ã†ã”ã–ã„ã¾ã™ï¼ï¼
    ã€€ã€€ã€€æ—¥æ­©è·¯ä¸»ã‚ã‚ŠãŒã¨ã†ã”ã–ã„ã¾ã™ï¼ï¼
    ï½
    
       ----
                            -   -   -
    DziÄ™kujÄ™ Polska!!
    
       Thomas Borregaard Sørensen dziÄ™kujÄ™!!
    
       Temp(TODO annotation) : Removing statically initialized variables. Leaving only "integer" types
                               static globals and "integer"->pointer types static globals with Sys(::get_thread_app())
    						   balm (please register at Sys[System] the initialized objects  so it can be deleted
    						   fashionly (you can take care of order,before alloc system is deleted and even between
    						   single tons, when dependency information is registered also (great IDEA : great TODO!!)
    
    +âˆž
    
    -âˆž
    +âˆž
    
    -âˆž
    +âˆž
    
    -âˆž
    Mummi contribution!!, eu te amo demais!!
    +âˆž
    
    -âˆž
    +âˆž
    
    âˆž-âˆž-âˆžâˆž-âˆž-âˆž
    -----8nnnnnnnn8
    ++++++++++
    âˆž+âˆž+âˆžâˆž+âˆž+âˆž
    
    -âˆž
    +âˆž
    
    -âˆž
    +âˆž

--- operational_system/system.h
+++ operational_system/system.h
@@ -17,8 +17,28 @@ class metrowin_todo { public: metrowin_todo() {}  ~metrowin_todo() {} };
 #pragma pointers_to_members( full_generality, virtual_inheritance )
 #endif
 
-#include "win32_system.h"
-#define _ApplicationFrameworkDLL
+
+#define _BIND_TO_CURRENT_CRT_VERSION 1
+#define _CRT_NOFORCE_MANIFEST
+#define _STL_NOFORCE_MANIFEST
+
+
+BEGIN_EXTERN_C
+
+
+__declspec(selectany)  int _forceCRTManifest;
+
+
+END_EXTERN_C
+
+
+#define NOTHROW throw()
+#define THROWS throw(...)
+
+
+#ifndef _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+#endif
 
 
 #if defined(_M_X64)
